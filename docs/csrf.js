053cc8209609bcba1deb2ed4e346946584384b7e
kotonekurita <kotone.kurita@di-v.co.jp>
2021-03-03 21:26:57 +0900

    Update csrf.js

--- docs/csrf.js
+++ docs/csrf.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
-	const iframe = document.getElementById("csrf-iframe");
-  const form = iframe.contentWindow.document.querySelector('#csrf-form');
+	// const iframe = document.getElementById("csrf-iframe");
+  // const form = iframe.contentWindow.document.querySelector('#csrf-form');
+  const form = document.querySelector('#csrf-form');
   form.submit()
 })
